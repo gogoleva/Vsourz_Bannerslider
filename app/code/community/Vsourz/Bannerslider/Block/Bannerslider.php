f52ba91969e645d0639c3de1915229807a178ba2
Nick Dilßner <nd@kirchbergerknorr.de>
2016-08-09 20:10:04 +0200

    #Add - Frontend  + (add) sorting possibility

--- app/code/community/Vsourz/Bannerslider/Block/Bannerslider.php
+++ app/code/community/Vsourz/Bannerslider/Block/Bannerslider.php
@@ -1,10 +1,31 @@
-<?php 
+<?php
+
+/**
+ * Class Vsourz_Bannerslider_Block_Bannerslider
+ *
+ * @method int	getCategoryId()	get configured category id
+ */
 class Vsourz_Bannerslider_Block_Bannerslider extends Mage_Catalog_Block_Product_Abstract{
-	public function getImages()
+
+	/**
+	 * get all images for configured category id
+	 *
+	 * @param	string									$order		(optional) order field name
+	 * @param	string									$direction	(optional) order direction
+	 *
+	 * @return	Vsourz_Bannerslider_Model_Imagedetail[]				images as collection
+	 */
+	public function getImages($order = 'slide_position', $direction = 'ASC')
 	{
 		$catId = $this->getCategoryId();
-		return $imageCollection = Mage::getModel('bannerslider/bannerslider')->getImageCollection($catId);
+		return $imageCollection = Mage::getModel('bannerslider/bannerslider')->getImageCollection($catId, $order, $direction);
 	}
+
+	/**
+	 * get category data for category id
+	 *
+	 * @return	Vsourz_Bannerslider_Model_Imagecategory[]			category data as collection
+	 */
 	public function getCategoryData()
 	{
 		$catId = $this->getCategoryId();
--- app/code/community/Vsourz/Bannerslider/Model/Bannerslider.php
+++ app/code/community/Vsourz/Bannerslider/Model/Bannerslider.php
@@ -1,17 +1,32 @@
 <?php
 class Vsourz_Bannerslider_Model_Bannerslider extends Mage_Core_Model_Abstract{
-	public function getImageCollection($catId){
-		$categoryId = $catId;
+	/**
+	 * get all images for category id
+	 *
+	 * @param	int										$catId		category id
+	 * @param	string									$order		(optional) order field name
+	 * @param	string									$direction	(optional) order direction
+	 * 
+	 * @return	Vsourz_Bannerslider_Model_Imagedetail[]				images as collection
+	 */
+	public function getImageCollection($catId, $order = 'bannerdetail_id', $direction = 'ASC'){
 		$imageCollection = Mage::getModel('bannerslider/imagedetail')->getCollection()
-		->addFieldToFilter('status','1')
-		->addFieldToFilter('slider_id',$categoryId);
+			->addFieldToFilter('status', '1')
+			->addFieldToFilter('slider_id', $catId)
+			->setOrder($order, $direction);
 		return $imageCollection;
 	}
+
+	/**
+	 * get category data for category id
+	 *
+	 * @param	int											$catId	category id
+	 * @return	Vsourz_Bannerslider_Model_Imagecategory[]			category data as collection
+	 */
 	public function getCategoryCollection($catId){
-		$categoryId = $catId;
 		$categoryCollection = Mage::getModel('bannerslider/imagecategory')->getCollection()		
-		->addFieldToFilter('status','1')
-		->addFieldToFilter('slider_id',$catId);
+			->addFieldToFilter('status', '1')
+			->addFieldToFilter('slider_id', $catId);
 		return $categoryCollection;
 	}
 }
